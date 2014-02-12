2a7f4373d864882e1ce2c44f01c79a467121da68
Jeff Gayle <jeffgayle@wilson.tpnevents.local>
2014-02-12 09:20:02 -0800

    Initial Commit

---
+++ Dollar ConverterTests/Dollar_ConverterTests.m
@@ -0,0 +1,34 @@
+//
+//  Dollar_ConverterTests.m
+//  Dollar ConverterTests
+//
+//  Created by Jeff Gayle on 2/12/14.
+//  Copyright (c) 2014 Jeff Gayle. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Dollar_ConverterTests : XCTestCase
+
+@end
+
+@implementation Dollar_ConverterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
